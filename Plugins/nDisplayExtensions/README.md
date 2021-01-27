3a908e62f82aca8c548b509b25173e8efd1f5d3f
Marius Schmeling <schmeling@vr.rwth-aachen.de>
2021-01-27 10:48:15 +0100

    Changing to only Plugin. Demo can befound at Study Framework Demo

--- Plugins/nDisplayExtensions/README.md
+++
@@ -1,3 +0,0 @@
-# nDisplayExtensions
-
-Unreal Engine nDisplay plugin extensions, including custom CAVE game modes, pawns and more.
--- Source/StudyFramework/StudyFramework.cpp
+++
@@ -1,4 +0,0 @@
-#include "StudyFramework.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StudyFramework, "StudyFramework" );

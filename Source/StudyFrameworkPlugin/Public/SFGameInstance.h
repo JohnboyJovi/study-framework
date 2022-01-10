ee4044b656501d723d1ab8ab3aa1df26bf27d13d
Ehret <jw210150@WIN.RZ.RWTH-AACHEN.DE>
2022-01-10 23:19:36 +0100

    add comments for USFGameInstance methods that should be used

--- Source/StudyFrameworkPlugin/Public/SFGameInstance.h
+++ Source/StudyFrameworkPlugin/Public/SFGameInstance.h
@@ -29,36 +29,57 @@ public:
 	virtual void Shutdown() override;
 	virtual void OnWorldChanged(UWorld* OldWorld, UWorld* NewWorld) override;
 
+
+	// getter for the GameInstance itself
 	UFUNCTION(BlueprintCallable,  meta = (DisplayName="Get SFGameInstance"))
 	static USFGameInstance* Get();
+	// a check method whether Init was called by the UE system, otherwise Get() cannot be called
 	static bool IsInitialized();
 
 	// ****************************************************************** // 
 	// ******* Control Study ******************************************** //
 	// ****************************************************************** //
+
+	//Start the study
 	UFUNCTION(BlueprintCallable)
 	bool StartStudy(const USFCondition* StartCondition = nullptr);
+
+	//End the study (called internally, not sure whether this shoul be used from outside)
 	UFUNCTION(BlueprintCallable)
-	void EndStudy(); // TODO implement EndStudy()
+	void EndStudy();
+
+	//Fade to the next condition (use this to proceed in the study once condition is done)
 	UFUNCTION(BlueprintCallable)
 	bool NextCondition();
+
+	//This method can be used to jump to a specific condition (DO NOT USE during normal study run)
 	UFUNCTION(BlueprintCallable)
 	bool GoToCondition(const USFCondition* Condition);
+
+	//Whether the study was started already
 	UFUNCTION(BlueprintCallable)
 	bool IsStarted() const;
 
+	//Get the level of a given factor in the current condition
+	//use this to configure everything in the scene for this condition
+	//Right Level is already accessible in BeginPlay
 	UFUNCTION(BlueprintCallable)
 	FString GetFactorLevel(FString FactorName) const;
 
+	//Log Data collected for a DependentVariable in this condition
 	UFUNCTION(BlueprintCallable)
 	void LogData(const FString& DependenVariableName, const FString& Value);
+
+	//Log a comment (with a timestamp) to store events or user action etc. in the participant's log file
 	UFUNCTION(BlueprintCallable)
 	void LogComment(const FString& Comment);
 
+	//use this to print something to the log panel of the HUD, e.g., to inform the experimenter
 	UFUNCTION()
 	void LogToHUD(FString Text);
 
-	
+	//A delegate to register to when you want to start behavior once the condition is faded in
+	//NOTE: BeginPlay is called while it is still faded out!
 	UPROPERTY(BlueprintAssignable)
 	FOnFadedInDelegate OnFadedInDelegate;
 
